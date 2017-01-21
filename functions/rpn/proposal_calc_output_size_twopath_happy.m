2af7e047c345439dd5a2372adf3362e2c1e6c59d
yumkong <yumkongleo@gmail.com>
2017-01-21 10:02:42 -0500

    minor changes to prepare anchors

---
+++ functions/rpn/proposal_calc_output_size_twopath_happy.m
@@ -0,0 +1,76 @@
+function [output_width_map_res23, output_height_map_res23, ...
+          output_width_map_res45, output_height_map_res45] = proposal_calc_output_size_twopath_happy(conf, test_net_def_file, output_map_save_name)
+% [output_width_map, output_height_map] = proposal_calc_output_size(conf, test_net_def_file)
+% --------------------------------------------------------
+% Faster R-CNN
+% Copyright (c) 2015, Shaoqing Ren
+% Licensed under The MIT License [see LICENSE for details]
+% --------------------------------------------------------
+
+%     caffe.init_log(fullfile(pwd, 'caffe_log'));
+    %0805 added
+    %cache_output_width_map = 'output_width_map_vgg16.mat';
+    %cache_output_height_map = 'output_height_map_vgg16.mat';
+
+    try
+        %load(cache_output_width_map);
+        %load(cache_output_height_map);
+        load(output_map_save_name);  %output_width_map, output_height_map
+    catch
+
+        caffe_net = caffe.Net(test_net_def_file, 'test');
+
+         % set gpu/cpu
+        if conf.use_gpu
+            caffe.set_mode_gpu();
+        else
+            caffe.set_mode_cpu();
+        end
+        %1209 changed
+        %input = 100:conf.max_size;
+        input = 512;  %100:1510
+        output_w_res23 = nan(size(input));
+        output_h_res23 = nan(size(input));
+        output_w_res45 = nan(size(input));
+        output_h_res45 = nan(size(input));
+
+        for i = 1:length(input)
+            fprintf('calulating input size %d / %d\n',i,conf.max_size);
+            % 1209 changed in order pool and deconv change dimension
+            s = input(i);
+            %s = ceil(input(i)/8)*8;
+            %liu@0926 changed,because when s>= 1000, s x s is too big to feed to a net
+            im_blob = single(zeros(s, s, 3, 1));
+            %im_blob = single(zeros(s, 504, 3, 1)); %1210: 500-->504
+            net_inputs = {im_blob};
+
+            % Reshape net's input blobs
+            caffe_net.reshape_as_input(net_inputs);
+            caffe_net.forward(net_inputs);
+
+            cls_score_conv34 = caffe_net.blobs('proposal_cls_score_res23').get_data();
+            % w x h x ch x num
+            output_w_res23(i) = size(cls_score_conv34, 1);
+            %liu@0926 changed, see above
+            output_h_res23(i) = size(cls_score_conv34, 1);
+            %output_h(i) = size(cls_score, 2);
+            
+            cls_score_conv5 = caffe_net.blobs('proposal_cls_score_res45').get_data();
+            % w x h x ch x num
+            output_w_res45(i) = size(cls_score_conv5, 1);
+            output_h_res45(i) = size(cls_score_conv5, 1);
+        end
+
+        output_width_map_res23 = containers.Map(input, output_w_res23);
+        output_height_map_res23 = containers.Map(input, output_h_res23);
+        output_width_map_res45 = containers.Map(input, output_w_res45);
+        output_height_map_res45 = containers.Map(input, output_h_res45);
+        %0805
+        %save(cache_output_width_map, 'output_width_map', '-v7.3');
+        %save(cache_output_height_map, 'output_height_map', '-v7.3');
+        %0925
+        save(output_map_save_name, 'output_width_map_res23', 'output_height_map_res23',...
+                'output_width_map_res45', 'output_height_map_res45');
+        caffe.reset_all(); 
+    end
+end
---
+++ functions/rpn/proposal_generate_anchors_twopath_flip.m
@@ -0,0 +1,87 @@
+function [anchors_res23, anchors_res45] = proposal_generate_anchors_twopath_flip(cache_name, varargin)
+% anchors = proposal_generate_anchors(cache_name, varargin)
+% --------------------------------------------------------
+% Faster R-CNN
+% Copyright (c) 2015, Shaoqing Ren
+% Licensed under The MIT License [see LICENSE for details]
+% --------------------------------------------------------
+
+%% inputs
+    ip = inputParser;
+    ip.addRequired('cache_name',                        @isstr);
+
+    % the size of the base anchor 
+    ip.addParamValue('base_size',       16,             @isscalar);
+    % ratio list of anchors
+    ip.addParamValue('ratios',          [1],            @ismatrix);%0807: [0.5, 1, 2] --> [1]
+    % scale list of anchors
+    ip.addParamValue('scales',          2.^[0:5],       @ismatrix); %0807: 2.^[3:5]-->2.^[0:5]
+    ip.addParamValue('add_size',        [900],          @ismatrix); %1112 a new param: add anchor size that is != 2^x
+    ip.addParamValue('ignore_cache',    false,          @islogical);
+    ip.parse(cache_name, varargin{:});
+    opts = ip.Results;
+
+%%
+    if ~opts.ignore_cache
+        %anchor_cache_dir            = fullfile(pwd, 'output', 'rpn_cachedir', cache_name); 
+%         anchor_cache_dir            = fullfile(pwd, 'cache_data', cache_name); 
+%                                       mkdir_if_missing(anchor_cache_dir);
+%         anchor_cache_file           = fullfile(anchor_cache_dir, 'anchors');
+        anchor_cache_file           = fullfile(cache_name, 'anchors');
+    end
+    try
+        ld                      = load(anchor_cache_file);
+        anchors                 = ld.anchors;
+    catch
+        base_anchor             = [1, 1, opts.base_size, opts.base_size];
+        ratio_anchors           = ratio_jitter(base_anchor, opts.ratios);
+        anchors                 = cellfun(@(x) scale_jitter(x, opts.scales, opts.add_size, opts.base_size), num2cell(ratio_anchors, 2), 'UniformOutput', false);
+        anchors                 = cat(1, anchors{:});
+        % 1112 added
+        half_anchor_num = size(anchors, 1) / 2;
+        anchors_res23 = anchors([1:3 half_anchor_num+1:half_anchor_num+3], :);  % [8  16 32]
+        anchors_res45 = anchors([4:7 half_anchor_num+4:half_anchor_num+7], :);  % [64 128 256 480]
+        if ~opts.ignore_cache
+            save(anchor_cache_file, 'anchors_res23', 'anchors_res45');
+        end
+    end
+    
+end
+
+function anchors = ratio_jitter(anchor, ratios)
+    ratios = ratios(:);
+    
+    w = anchor(3) - anchor(1) + 1;
+    h = anchor(4) - anchor(2) + 1;
+    x_ctr = anchor(1) + (w - 1) / 2;
+    y_ctr = anchor(2) + (h - 1) / 2;
+    size = w * h;
+    
+    size_ratios = size ./ ratios;
+    ws = round(sqrt(size_ratios));
+    hs = round(ws .* ratios);
+    
+    anchors = [x_ctr - (ws - 1) / 2, y_ctr - (hs - 1) / 2, x_ctr + (ws - 1) / 2, y_ctr + (hs - 1) / 2];
+end
+
+function anchors = scale_jitter(anchor, scales, add_size, base_size)
+    scales = scales(:);
+    % convert additional anchor-size to anchor-ratio and append to the
+    % existing 2^x ratios and sort in ascending order
+    scales = sort([scales; (add_size / base_size)']);
+    w = anchor(3) - anchor(1) + 1;
+    h = anchor(4) - anchor(2) + 1;
+    x_ctr = anchor(1) + (w - 1) / 2;
+    y_ctr = anchor(2) + (h - 1) / 2;
+
+    ws = w * scales;
+    hs = h * scales;
+    %ws = [w * scales; add_size];
+    %hs = [h * scales; add_size];
+    % 1121: in ascending order
+    %ws = sort([w * scales; add_size']);
+    %hs = sort([h * scales; add_size']);
+    
+    anchors = [x_ctr - (ws - 1) / 2, y_ctr - (hs - 1) / 2, x_ctr + (ws - 1) / 2, y_ctr + (hs - 1) / 2];
+end
+
