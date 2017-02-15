7909094ad5f818b57a8045d6f0428e8b7305818c
yumkong <yumkongleo@gmail.com>
2017-02-14 21:02:22 -0500

    add new concat models

---
+++ experiments/+Model/VGG16_for_multibox_batch2_ablation_conv345_s4.m
@@ -0,0 +1,19 @@
+function model = VGG16_for_multibox_batch2_ablation_conv345_s4(exp_name, model)
+
+
+model.mean_image                                = fullfile(pwd, 'models', exp_name, 'pre_trained_models', 'vgg_16layers', 'mean_image');
+model.pre_trained_net_file                      = fullfile(pwd, 'models', exp_name, 'pre_trained_models', 'vgg_16layers', 'vgg16.caffemodel');
+% Stride in input image pixels at the last conv layer
+model.feat_stride                               = 4;
+
+%% stage 1 rpn, inited from pre-trained network
+model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation', 'stride4_exp', 'solver_30k40k_conv345_concat.prototxt'); 
+model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation', 'stride4_exp', 'test_conv345_concat.prototxt');
+model.stage1_rpn.init_net_file                  = model.pre_trained_net_file;
+
+% rpn test setting
+model.stage1_rpn.nms.per_nms_topN               = -1; %20000
+model.stage1_rpn.nms.nms_overlap_thres       	= 0.7;%0111: should also try 0.7 when doing testing
+model.stage1_rpn.nms.after_nms_topN         	= 300;  %1204:800--100
+
+end
