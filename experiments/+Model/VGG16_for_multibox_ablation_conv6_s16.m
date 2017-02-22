4c9b310bc257763bbe0986274b5cc9c22aadf553
yumkong <yumkongleo@gmail.com>
2017-02-22 16:09:57 -0500

    minor changes in vn7

--- experiments/+Model/VGG16_for_multibox_ablation_conv6_s16.m
+++ experiments/+Model/VGG16_for_multibox_ablation_conv6_s16.m
@@ -7,8 +7,8 @@ model.pre_trained_net_file                      = fullfile(pwd, 'models', exp_na
 model.feat_stride                               = 16;%4
 
 %% stage 1 rpn, inited from pre-trained network
-model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation', 'solver_30k40k_conv6_stride16.prototxt'); 
-model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation', 'test_conv6_stride16.prototxt');
+model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation','stride16_exp', 'solver_30k40k_conv6_stride16.prototxt'); 
+model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_16layers_ablation','stride16_exp', 'test_conv6_stride16.prototxt');
 model.stage1_rpn.init_net_file                  = model.pre_trained_net_file;
 
 % rpn test setting
