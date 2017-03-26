be82c535ac273e658ff2ca5990babae7c68d5ec9
yumkong <yumkongleo@gmail.com>
2017-03-26 09:46:00 -0400

    minor change to fix a bug

--- experiments/script_VGG16_widerface_ablation_fastrcnn_mpfpn_cxt.m
+++ experiments/script_VGG16_widerface_ablation_fastrcnn_mpfpn_cxt.m
@@ -103,7 +103,7 @@ end
 
 % %% -------------------- TRAIN --------------------
 % conf
-conf_proposal          = proposal_config_widerface_ablation_total('image_means', model.mean_image, 'feat_stride_s4', model.feat_stride_s4,...
+conf_proposal          = proposal_config_widerface_mpfvn('image_means', model.mean_image, 'feat_stride_s4', model.feat_stride_s4,...
                                                                     'feat_stride_s8', model.feat_stride_s8, 'feat_stride_s16', model.feat_stride_s16);
 conf_fast_rcnn              = fast_rcnn_config_widerface_mpfvn('image_means', model.mean_image);
 % generate anchors and pre-calculate output size of rpn network 
