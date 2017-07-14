e2ea5bf6c549636cd3324889ecb14a7004e36f13
yumkong <yumkongleo@gmail.com>
2017-07-14 18:30:51 -0400

    add conv3 2d

---
+++ experiment2/+rpn/get_prototxt_conv3_2d.m
@@ -0,0 +1,19 @@
+function model = get_prototxt_conv3_2d(exp_name, model)
+
+
+model.mean_image                                = fullfile(pwd, 'models', exp_name, 'pre_trained_models', 'vgg_16layers', 'mean_image');
+model.pre_trained_net_file                      = fullfile(pwd, 'models', exp_name, 'pre_trained_models', 'vgg_16layers', 'vgg16.caffemodel');
+% Stride in input image pixels at the last conv layer
+model.feat_stride                               = 4;
+
+%% stage 1 rpn, inited from pre-trained network
+model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d', 'solver_30k40k_conv3_2d.prototxt'); 
+model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d', 'test_conv3_2d.prototxt');
+model.stage1_rpn.init_net_file                  = model.pre_trained_net_file;
+
+% rpn test setting
+model.stage1_rpn.nms.per_nms_topN               = -1; %20000
+model.stage1_rpn.nms.nms_overlap_thres       	= 0.7;%0111: should also try 0.7 when doing testing
+model.stage1_rpn.nms.after_nms_topN         	= 300;  %1204:800--100
+
+end
