fcdb6dd6819e253c66e2324c6c18196bc8986a58
yumkong <yumkongleo@gmail.com>
2017-08-09 22:18:41 -0400

    commit from puck

--- experiment2/+rpnmodel/get_prototxt_conv3_2d_centerloss.m
+++ experiment2/+rpnmodel/get_prototxt_conv3_2d_centerloss.m
@@ -8,8 +8,9 @@ model.feat_stride                               = 4;
 
 %% stage 1 rpn, inited from pre-trained network
 %%conv3_2d_centerloss + solver_30k40k_conv3_2d_new => with center_loss, conv3_2d + solver_30k40k_conv3_2d => without centerloss
-model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d_centerloss', 'solver_30k40k_conv3_2d_new.prototxt'); 
-model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d_centerloss', 'test_conv3_2d_new.prototxt');
+%  solver_30k40k_conv3_2d_nopermute => centerloss withou permute
+model.stage1_rpn.solver_def_file                = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d_centerloss', 'solver_30k40k_conv3_2d_nopermute.prototxt'); 
+model.stage1_rpn.test_net_def_file              = fullfile(pwd, 'models', exp_name, 'rpn_prototxts', 'vgg_leave','conv3_2d_centerloss', 'test_conv3_2d_nopermute.prototxt');
 model.stage1_rpn.init_net_file                  = model.pre_trained_net_file;
 
 % rpn test setting
